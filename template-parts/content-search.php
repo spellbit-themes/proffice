0a2428ad2c8ab8ffb2d6b04436d79ae79db571c5
mahedi amin <mahedi_cse@hotmail.com>
2018-06-26 12:41:18 +0600

    search page css fix again

--- template-parts/content-search.php
+++ template-parts/content-search.php
@@ -10,7 +10,7 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(array('single-blog', 'innerpage', 'Fullpage')); ?>>
-	<div class="blog-content">
+	<div class="blog-content search">
 		<?php the_title( sprintf( '<h2><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
 
 		<?php if ( 'post' === get_post_type() ) : ?>
