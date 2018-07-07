9535679e70ab888501988e698a01b6d0d650f9d3
akramul hasan <dev.akramul@gmail.com>
2018-07-07 09:43:11 +0600

    fixed page comment box issue

--- template-parts/content-page.php
+++ template-parts/content-page.php
@@ -42,7 +42,7 @@
 			);
 
 // If comments are open or we have at least one comment, load up the comment template.
-        			if ( comments_open() || get_comments_number() ) :
+        			if ( comments_open()) :
         				comments_template();
         			endif;
         			?>
